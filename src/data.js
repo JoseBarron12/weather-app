377f46e6f371b59f9514171ae97b930aeffab157
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-10-09 18:36:31 -0500

    Add methods to WeatherData class for header info.
    Methods include getting info for location name,
    current temperature, condition, min+max temps for
    displaying info for header of page.

--- src/data.js
+++ src/data.js
@@ -7,6 +7,26 @@ class WeatherData {
     get data() {
         return this._data
     }
+
+    getLocationName() {
+        return this._data.resolvedAddress;
+    }
+
+    getCurrentTemp() {
+        return Math.round(this._data.currentConditions.temp);
+    }
+
+    getCurrentCondition() {
+        return this._data.currentConditions.conditions;
+    }
+
+    getMaxTemp() {
+        return Math.round(this._data.days[0].tempmax);
+    }
+
+    getMinTemp() {
+        return Math.round(this._data.days[0].tempmin);
+    }
 }
 
 export {WeatherData}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import "./styles.css";
 import { WeatherData } from "./data";
-
+/*
 const getWeather = async () => {
     const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/palatine?key=58BNGSSDKDUY7PYBE3E3WZ3TV");
     const weatherData = await response.json();
     const weatherClass = new WeatherData(weatherData);
-    console.log(weatherClass.data);
+    return weatherClass;
 }
-getWeather();
+const weather = await getWeather();
+
+console.log(weather.data);
+
+console.log(weather.getLocationName());
+
+console.log(weather.getCurrentTemp());
+
+console.log(weather.getCurrentCondition());
+
+console.log(weather.getMaxTemp());
+
+console.log(weather.getMinTemp());
+
+*/ 
