eacd11d7e61e1817e1067133a5c3aff489fc314f
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-10-09 16:27:05 -0500

    Add async function to get weather data.
    Weather data logged to console to view for future
    implementation.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles.css";
 
+const getWeather = async () => {
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/chicago?key=58BNGSSDKDUY7PYBE3E3WZ3TV");
+    const weatherData = await response.json();
+    console.log(weatherData);
+}
+
+getWeather();
