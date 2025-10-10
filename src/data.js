2be6642ca275f848058cf7d7211d3454886ad007
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-10-10 17:19:17 -0500

    Add get method for all relevant curr. weather data
    WeatherData class has all methods for getting
    info that will be displayed on webpage from
    current fetch url. Air quality index and averages
    obtained with seperate url for future implementation.

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+import { fromUnixTime } from "date-fns";
+
 class WeatherData {
     constructor(data)
     {
@@ -27,6 +29,73 @@ class WeatherData {
     getMinTemp() {
         return Math.round(this._data.days[0].tempmin);
     }
+
+    getDescription() {
+        return this._data.description;
+    }
+
+    getCurrentDateTime() {
+        return fromUnixTime(this._data.currentConditions.datetimeEpoch);
+    }
+
+    getSunrise(){
+        return fromUnixTime(this._data.currentConditions.sunriseEpoch);
+    }
+
+    getSunset() {
+        return fromUnixTime(this._data.currentConditions.sunsetEpoch);
+    }
+
+    getFeelsLikeTemp() {
+        return Math.round(this._data.currentConditions.feelslike);
+    }
+
+    getUVIndex() {
+        return this._data.currentConditions.uvindex;
+    }
+
+    getWindSpeed() {
+        return this._data.currentConditions.windspeed;
+    }
+
+    getWindGust() {
+        return this._data.currentConditions.windgust;
+    }
+
+    getWindDirection() {
+        return this._data.currentConditions.winddir;
+    }
+
+    getAmountOfPrecipitation() {
+        return this._data.currentConditions.precip;
+    }
+
+    getChanceOfPrecipitation() {
+        return this._data.currentConditions.precipprob;
+    }
+
+    getTypeOfPrecipitation() {
+        return this._data.currentConditions.preciptype;
+    }
+
+    getVisibility() {
+        return this._data.currentConditions.visibility;
+    }
+
+    getHumidity() {
+        return Math.round(this._data.currentConditions.humidity);
+    }
+
+    getDewPt() {
+        return Math.round(this._data.currentConditions.dew);
+    }
+
+    getPressure() {
+        return this._data.currentConditions.pressure
+    }
+    
 }
+
+
 
 export {WeatherData}
--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ const savedWeatherData = JSON.stringify(weatherData);
 
 localStorage.setItem("currentWeather", savedWeatherData);*/
 
-/* 
 
+/*
 const getWeather = async () => {
     const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/palatine?key=58BNGSSDKDUY7PYBE3E3WZ3TV");
     const weatherData = await response.json();
@@ -37,15 +37,17 @@ const getWeather = async () => {
     localStorage.setItem("currentWeather", savedWeatherData);
 }
 
-getWeather();*/
+getWeather();
+
+ */
 
 const weatherSavedData = JSON.parse(localStorage.getItem("currentWeather"));
 
 const weather = new WeatherData(weatherSavedData);
 
-display.header(weather);
-
 console.log(weather.data);
+
+display.header(weather);
 
 console.log(weather.getLocationName());
 
@@ -56,5 +58,37 @@ console.log(weather.getCurrentCondition());
 console.log(weather.getMaxTemp());
 
 console.log(weather.getMinTemp());
+
+console.log(weather.getDescription());
+
+console.log(weather.getCurrentDateTime());
+
+console.log(weather.getSunrise());
+
+console.log(weather.getSunset());
+
+console.log(weather.getFeelsLikeTemp());
+
+console.log(weather.getUVIndex());
+
+console.log(weather.getWindSpeed());
+
+console.log(weather.getWindDirection());
+
+console.log(weather.getWindGust());
+
+console.log(weather.getAmountOfPrecipitation());
+
+console.log(weather.getChanceOfPrecipitation());
+
+console.log(weather.getTypeOfPrecipitation());
+
+console.log(weather.getVisibility());
+
+console.log(weather.getHumidity());
+
+console.log(weather.getDewPt());
+
+console.log(weather.getPressure());
 
 
