82859f39e2b88e0842c584d4a85765ee9a4aa291
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-10-21 17:06:54 -0500

    Update default display, and circle section.
    Default display no longer html content but the
    first user location, and circle section no longer
    always shows 5 circles but based on number of user
    selected locations.

--- src/index.js
+++ src/index.js
@@ -31,49 +31,6 @@ slider.addEventListener('mousedown', startDragging, false);
 slider.addEventListener('mouseup', stopDragging, false);
 slider.addEventListener('mouseleave', stopDragging, false);
 
-const circleSection = document.querySelector(".page-slider");
-
-for(let i = 0; i < 5; i++)
-{
-    if(i === 0) {
-        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttribute("class", "circle current-circle");
-        svg.setAttribute("viewBox", "0 0 24 24");
-        svg.setAttribute("width", "100");
-        svg.setAttribute("height", "100");
-
-
-        const title = document.createElementNS("http://www.w3.org/2000/svg", "title");
-        title.textContent = "circle";
-        svg.appendChild(title);
-
-        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute("d", "M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z");
-        svg.appendChild(path);
-
-        circleSection.appendChild(svg);
-    }
-    else
-    {
-        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttribute("class", "circle");
-        svg.setAttribute("viewBox", "0 0 24 24");
-        svg.setAttribute("width", "100");
-        svg.setAttribute("height", "100");
-
-
-        const title = document.createElementNS("http://www.w3.org/2000/svg", "title");
-        title.textContent = "circle";
-        svg.appendChild(title);
-
-        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute("d", "M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z");
-        svg.appendChild(path);
-
-        circleSection.appendChild(svg);
-    }
-}
-
 //callAPI.allWeatherData("palatine");
 
 function getSkyGradient(time, sunrise = 6, sunset = 18, isClear = true) {
@@ -228,7 +185,9 @@ console.log(weatherDatas);
 
 const locations = document.querySelector(".locations");
 
-weatherDatas.forEach((data) => {
+
+weatherDatas.forEach((data, index) => {
+  
   const weatherData = new WeatherData(data._data)
   weatherData.setMoonDuration(data._moonDuration);
   weatherData.setMoonPhase(data._moonPhase);
@@ -240,4 +199,53 @@ weatherDatas.forEach((data) => {
 
   console.log(weatherData);
   display.location(weatherData,locations,dialog);
+
+  if(index == 0)
+  {
+    display.fullPage(weatherData);
+  }
+
 });
+
+const circleSection = document.querySelector(".page-slider");
+
+for(let i = 0; i < weatherDatas.length; i++)
+{
+    if(i === 0) {
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("class", "circle current-circle");
+        svg.setAttribute("viewBox", "0 0 24 24");
+        svg.setAttribute("width", "100");
+        svg.setAttribute("height", "100");
+
+
+        const title = document.createElementNS("http://www.w3.org/2000/svg", "title");
+        title.textContent = "circle";
+        svg.appendChild(title);
+
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path.setAttribute("d", "M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z");
+        svg.appendChild(path);
+
+        circleSection.appendChild(svg);
+    }
+    else
+    {
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("class", "circle");
+        svg.setAttribute("viewBox", "0 0 24 24");
+        svg.setAttribute("width", "100");
+        svg.setAttribute("height", "100");
+
+
+        const title = document.createElementNS("http://www.w3.org/2000/svg", "title");
+        title.textContent = "circle";
+        svg.appendChild(title);
+
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path.setAttribute("d", "M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z");
+        svg.appendChild(path);
+
+        circleSection.appendChild(svg);
+    }
+}
